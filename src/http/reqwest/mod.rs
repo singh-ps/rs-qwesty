0585d580008a2d71e7ae7567ba14d22ac4ce4526
Pushpinder Singh <pushpindersingh89@hotmail.com>
2025-07-20 21:21:04 +0530

    feat: Add POST and DELETE

--- src/http/reqwest/mod.rs
+++ src/http/reqwest/mod.rs
@@ -1,7 +1,11 @@
 mod client;
+mod delete;
 mod get;
+mod post;
 mod put;
 
 pub use client::get_client;
+pub use delete::delete;
 pub use get::get;
+pub use post::{post, post_empty};
 pub use put::put;
